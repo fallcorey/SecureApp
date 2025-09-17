cc56546e042f6b0016c48a2d91d31c51d17e017e
fallcorey <fallcorey.a@gmail.com>
2025-09-17 20:32:00 +0200

    Update MainActivity.kt

--- app/src/main/java/com/company/secureapp/MainActivity.kt
+++ app/src/main/java/com/company/secureapp/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.company.secureapp
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         val sosButton = findViewById<Button>(R.id.sos_button)
         val settingsButton = findViewById<Button>(R.id.settings_button)
-        
+
         sosButton.setOnClickListener {
             Toast.makeText(this, "SOS button clicked", Toast.LENGTH_SHORT).show()
         }
-        
+
         settingsButton.setOnClickListener {
             Toast.makeText(this, "Settings button clicked", Toast.LENGTH_SHORT).show()
         }
