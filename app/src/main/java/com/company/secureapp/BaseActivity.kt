6188bacc6c649a9800e10571ee71c6a619c58e37
fallcorey <fallcorey.a@gmail.com>
2025-09-20 16:02:35 +0200

    Update BaseActivity.kt

--- app/src/main/java/com/company/secureapp/BaseActivity.kt
+++ app/src/main/java/com/company/secureapp/BaseActivity.kt
@@ -1,4 +1,4 @@
-package com.fallcorey.secureapp
+package com.company.secureapp
 
 import android.content.Context
 import android.os.Bundle
@@ -7,20 +7,20 @@ import androidx.appcompat.app.AppCompatActivity
 open class BaseActivity : AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context) {
-        val localeManager = com.fallcorey.secureapp.utils.LocaleManager
+        val localeManager = com.company.secureapp.utils.LocaleManager
         super.attachBaseContext(localeManager.setLocale(newBase, localeManager.getCurrentLanguage(newBase)))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Применяем язык сразу при создании активности
-        com.fallcorey.secureapp.utils.LocaleManager.setLocale(this, 
-            com.fallcorey.secureapp.utils.LocaleManager.getCurrentLanguage(this))
+        com.company.secureapp.utils.LocaleManager.setLocale(this, 
+            com.company.secureapp.utils.LocaleManager.getCurrentLanguage(this))
     }
 
     fun changeLanguage(languageCode: String) {
-        com.fallcorey.secureapp.utils.LocaleManager.saveLanguage(this, languageCode)
-        com.fallcorey.secureapp.utils.LocaleManager.setLocale(this, languageCode)
+        com.company.secureapp.utils.LocaleManager.saveLanguage(this, languageCode)
+        com.company.secureapp.utils.LocaleManager.setLocale(this, languageCode)
         
         // Пересоздаем активность для применения изменений
         recreate()
