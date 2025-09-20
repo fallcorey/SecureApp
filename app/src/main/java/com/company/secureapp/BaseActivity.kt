7be3e72aa454b1d1badcc7b4c75b0ccf393a3b1f
fallcorey <fallcorey.a@gmail.com>
2025-09-20 16:12:06 +0200

    Update BaseActivity.kt

--- app/src/main/java/com/company/secureapp/BaseActivity.kt
+++ app/src/main/java/com/company/secureapp/BaseActivity.kt
@@ -7,20 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 open class BaseActivity : AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context) {
-        val localeManager = com.company.secureapp.utils.LocaleManager
-        super.attachBaseContext(localeManager.setLocale(newBase, localeManager.getCurrentLanguage(newBase)))
+        super.attachBaseContext(LocaleManager.setLocale(newBase, LocaleManager.getCurrentLanguage(newBase)))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Применяем язык сразу при создании активности
-        com.company.secureapp.utils.LocaleManager.setLocale(this, 
-            com.company.secureapp.utils.LocaleManager.getCurrentLanguage(this))
+        LocaleManager.setLocale(this, LocaleManager.getCurrentLanguage(this))
     }
 
     fun changeLanguage(languageCode: String) {
-        com.company.secureapp.utils.LocaleManager.saveLanguage(this, languageCode)
-        com.company.secureapp.utils.LocaleManager.setLocale(this, languageCode)
+        LocaleManager.saveLanguage(this, languageCode)
+        LocaleManager.setLocale(this, languageCode)
         
         // Пересоздаем активность для применения изменений
         recreate()
