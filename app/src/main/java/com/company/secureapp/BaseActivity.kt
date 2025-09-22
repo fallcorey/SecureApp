71109aa21ba32759f405848b357a438d57f974ac
fallcorey <fallcorey.a@gmail.com>
2025-09-22 09:52:27 +0200

    Update BaseActivity.kt

--- app/src/main/java/com/company/secureapp/BaseActivity.kt
+++ app/src/main/java/com/company/secureapp/BaseActivity.kt
@@ -1,18 +1,31 @@
 package com.company.secureapp
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fallcorey.secureapp.utils.LocaleManager
 
 open class BaseActivity : AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context) {
-        super.attachBaseContext(newBase)
+        super.attachBaseContext(LocaleManager.setLocale(newBase, LocaleManager.getCurrentLanguage(newBase)))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Применяем язык сразу при создании активности
+        LocaleManager.setLocale(this, LocaleManager.getCurrentLanguage(this))
+    }
+
+    fun changeLanguage(languageCode: String) {
+        LocaleManager.saveLanguage(this, languageCode)
+        // Немедленно применяем изменения языка
+        LocaleManager.updateLanguage(this, languageCode)
+        
+        // Обновляем контент активности
+        recreate()
     }
 
     // Вспомогательные методы для Toast
@@ -22,24 +35,5 @@ open class BaseActivity : AppCompatActivity() {
 
     protected fun showToast(stringResId: Int) {
         Toast.makeText(this, getString(stringResId), Toast.LENGTH_LONG).show()
-    }
-
-    // Добавлен метод showError для совместимости
-    protected fun showError(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
-
-    protected fun showError(stringResId: Int) {
-        Toast.makeText(this, getString(stringResId), Toast.LENGTH_LONG).show()
-    }
-
-    // Добавляем метод changeLanguage
-    fun changeLanguage(languageCode: String) {
-        // Сохраняем выбранный язык в SharedPreferences
-        val preferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
-        preferences.edit().putString("selected_language", languageCode).apply()
-        
-        // Перезагружаем активность для применения языка
-        recreate()
     }
 }
